3e45050c972b984f914a132257898f13ab734bfa
hyedinion <ja03129@naver.com>
2021-11-19 02:25:33 +0900

    DP 합분해

--- DP/BOJ_합분해/합분해.py
+++ DP/BOJ_합분해/합분해.py
@@ -1,11 +1,8 @@
 N,K = map(int,input().split())
-DP = [[0]*(N+1) for _ in range(K)]
+DP = [[1]*(N+1) for _ in range(K)]
 
-for i in range(K):
-    for j in range(N+1):
-        if i==0:
-            DP[i][j] = 1
-        else:
-            DP[i][j] = DP[i][j-1]+DP[i-1][j]
+for i in range(1,K):
+    for j in range(1,N+1):
+        DP[i][j] = DP[i][j-1]+DP[i-1][j]
 
-print(DP[N])
+print(DP[K-1][N]%1000000000)
