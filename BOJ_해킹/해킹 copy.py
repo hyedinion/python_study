bb73e6cb542e0c77028ee8fc7ea715a019af1375
hyedinion <ja03129@naver.com>
2021-12-09 13:00:11 +0900

    해킹

--- BOJ_해킹/해킹 copy.py
+++ BOJ_해킹/해킹 copy.py
@@ -1,24 +1,8 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-T = int(input())
-for t in range(T):
-    N,D,C = map(int,input().split());C-=1
-    parent = [[]for _ in range(N)]
-    time = [9999 for _ in range(N)]
-    for d in range(D):
-        a,b,s = map(int,input().split());a-=1;b-=1
-        parent[b].append([a,s])
-    queue = deque()
-    queue.append(C)
-    time[C]=0
-    while(len(queue)!=0):
-        num = queue.popleft()
-        for p in parent[num]:
-            a,s = p
-            time[a] = min(time[a],time[num]+s)
-            queue.append(a)
 
+def check_answer(time):
     answer = 0
     answerCnt = 0
     for i in time:
@@ -26,4 +10,30 @@ for t in range(T):
             answerCnt+=1
             answer = max(answer,i)
     print(answerCnt,answer)
+    return
 
+def dijkstra(edge,time,C):
+    queue = deque()
+    queue.append(C)
+    time[C]=0
+
+    while(len(queue)!=0):
+        cur = queue.popleft()
+        for a,s in edge[cur]:
+            temp = time[cur]+s
+            if temp<time[a]:
+                time[a] = temp
+                queue.append(a)
+    return
+
+T = int(input())
+for t in range(T):
+    N,D,C = map(int,input().split());C-=1
+    edge = [[]for _ in range(N)]
+    time = [9999 for _ in range(N)]
+    for d in range(D):
+        a,b,s = map(int,input().split());a-=1;b-=1
+        edge[b].append([a,s])
+
+    dijkstra(edge,time,C)
+    check_answer(time)
